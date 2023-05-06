81d94e48b8240947b754c640f20badc788c72f60
DanielArcentales <alencastrobi@gmail.com>
2023-05-06 17:41:02 -0500

    Proyecto base java

---
+++ ejercicio_java/Actividad2/src/main/java/actividad2/Actividad2.java
@@ -0,0 +1,16 @@
+package actividad2;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Diego Alencastro
+ */
+public class Actividad2 {
+
+    public static void main(String[] args) {
+
+    }
+}
