7ab17e546c0e09b58119be3c84ba1090731a5386
DanielArcentales <alencastrobi@gmail.com>
2023-05-06 18:05:15 -0500

    Menu de control de opciones

--- ejercicio_java/Actividad2/src/main/java/actividad2/Actividad2.java
+++ ejercicio_java/Actividad2/src/main/java/actividad2/Actividad2.java
@@ -1,8 +1,6 @@
 package actividad2;
 
-import java.util.List;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 /**
  *
@@ -11,6 +9,33 @@ import java.util.ArrayList;
 public class Actividad2 {
 
     public static void main(String[] args) {
+        int menuPrincipal;
+        Scanner scannerEnteros = new Scanner(System.in);
 
+        do {
+            System.out.println("");
+            System.out.println("--SISTEMA DE GESTIÓN DE UNIVERSIDADES--");
+            System.out.println("1.Lista de Universidades registradas");
+            System.out.println("2.Agregar Universidad");
+            System.out.println("3.SALIR");
+            System.out.println("Escoja una opción:");
+            menuPrincipal = scannerEnteros.nextInt();
+
+            switch (menuPrincipal) {
+                case 1:
+                    System.out.println("--LISTADO DE UNIVERSIDADES REGISTRADAS--");
+
+                    break;
+                case 2:
+                    System.out.println("REGISTRO DE NUEVA UNIVERSIDAD");
+                    break;
+                case 3:
+                    System.out.println("---Gracias por usar nuestro sistema---");
+                    break;
+                default:
+                    System.out.println("OPCIÓN INVÁLIDA");
+            }
+
+        } while (menuPrincipal != 3);
     }
 }
